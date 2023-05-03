fb717687ecfa59dfaad68d1d981728ea08643660
Daniel Herszenhut <dhersz@gmail.com>
2023-05-03 13:59:31 -0300

    targets to build transport network

--- R/1_calculate_matrix.R
+++ R/1_calculate_matrix.R
@@ -18,3 +18,43 @@ generate_od_points <- function(grid_path) {
   
   centroids[]
 }
+
+# gtfs_fetranspor_url <- tar_read(gtfs_fetranspor_url)
+# gtfs_supervia_url <- tar_read(gtfs_supervia_url)
+# street_network_url <- tar_read(rio_street_network_url)
+# topography_url <- tar_read(rio_topography_url)
+build_transport_network <- function(gtfs_fetranspor_url,
+                                    gtfs_supervia_url,
+                                    street_network_url,
+                                    topography_url) {
+  if (!dir.exists("r5")) dir.create("r5")
+  
+  gtfs_fetransport_path <- file.path("r5", "gtfs_fetranspor.zip")
+  httr::GET(
+    gtfs_fetranspor_url,
+    httr::write_disk(gtfs_fetransport_path, overwrite = TRUE)
+  )
+  
+  gtfs_supervia_path <- file.path("r5", "gtfs_supervia.zip")
+  httr::GET(
+    gtfs_supervia_url,
+    httr::write_disk(gtfs_supervia_path, overwrite = TRUE)
+  )
+  
+  street_network_path <- file.path("r5", "rio_street_network.osm.pbf")
+  httr::GET(
+    street_network_url,
+    httr::write_disk(street_network_path, overwrite = TRUE)
+  )
+  
+  topography_path <- file.path("r5", "rio_topography.tif")
+  httr::GET(topography_url, httr::write_disk(topography_path, overwrite = TRUE))
+  
+  r5r::setup_r5(
+    "r5",
+    verbose = getOption("VERBOSE_R5R", default = FALSE),
+    overwrite = TRUE
+  )
+  
+  file.path("r5")
+}
--- _targets.R
+++ _targets.R
@@ -1,7 +1,8 @@
 options(
   java.parameters = "-Xmx50G",
   N_CORES = 30L,
-  SHOW_R5R_PROGRESS = TRUE
+  SHOW_R5R_PROGRESS = TRUE,
+  VERBOSE_R5R = FALSE
 )
 
 suppressPackageStartupMessages({
@@ -15,7 +16,37 @@ list(
   # data targets
   tar_target(rio_grid, "data-raw/rio_grid.rds", format = "file"),
   tar_target(rio_fare_structure, "data-raw/rio_fares.zip", format = "file"),
+  tar_target(od_points, generate_od_points(rio_grid)),
+  tar_target(
+    gtfs_fetranspor_url,
+    "https://github.com/ipeaGIT/pareto_importance_paper/releases/download/data/gtfs_fetranspor.zip",
+    format = "url"
+  ),
+  tar_target(
+    gtfs_supervia_url,
+    "https://github.com/ipeaGIT/pareto_importance_paper/releases/download/data/gtfs_supervia.zip",
+    format = "url"
+  ),
+  tar_target(
+    rio_street_network_url,
+    "https://github.com/ipeaGIT/pareto_importance_paper/releases/download/data/rio_street_network.osm.pbf",
+    format = "url"
+  ),
+  tar_target(
+    rio_topography_url,
+    "https://github.com/ipeaGIT/pareto_importance_paper/releases/download/data/rio_topography.tif",
+    format = "url"
+  ),
   
   # code-generated targets
-  tar_target(od_points, generate_od_points(rio_grid))
+  tar_target(
+    routing_dir,
+    build_transport_network(
+      gtfs_fetranspor_url,
+      gtfs_supervia_url,
+      rio_street_network_url,
+      rio_topography_url
+    ),
+    format = "file"
+  )
 )
